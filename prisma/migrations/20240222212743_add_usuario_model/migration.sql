f5ec337ce275d8590c9a4c7039aa731f61a7c3f2
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2024-02-22 22:32:45 +0100

    [Fix] : Changes in prisma model, added Usuario and install jsonwebtoken and bcryptjs

---
+++ prisma/migrations/20240222212743_add_usuario_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nombre" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
